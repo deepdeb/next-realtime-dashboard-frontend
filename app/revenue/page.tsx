5ea5d9a8b3788c7ee291d4b03855be2d08a27f55
deepdeb <debnath.pradeep619@gmail.com>
2025-08-10 23:30:43 +0530

    deleted dashboardcards.tsx and fixed revenue/page.tsx to not use non null assertion and chaining operators together

--- app/revenue/page.tsx
+++ app/revenue/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { useSocketData } from "@/context/SocketDataContext"
 
 export default function Revenue() {
@@ -7,6 +6,10 @@ export default function Revenue() {
     const totalRevenue = data?.reduce((acc, curr) => acc + curr.totalRevenue, 0);
     const totalProfits = data?.reduce((acc, curr) => acc + curr.profit, 0);
     const totalLoss = data?.reduce((acc, curr) => acc + curr.loss, 0);
+
+    const changeTotalRevenue = percentageChange?.changeTotalRevenue ?? 0;
+    const changeProfit = percentageChange?.changeProfit ?? 0;
+    const changeLoss = percentageChange?.changeLoss ?? 0;
 
     return (
         <div className="flex flex-col items-center space-y-5 w-full p-10">
@@ -31,15 +34,15 @@ export default function Revenue() {
             </div>
 
             <div className="rounded-xl bg-zinc-100 p-4 w-full">
-                {percentageChange?.changeTotalRevenue! > 0 ? (
+                {changeTotalRevenue > 0 ? (
                     <p className="text-center text-green-600">
                         Total Revenue Increased by&nbsp;
-                        {percentageChange?.changeTotalRevenue.toFixed(2)}%
+                        {changeTotalRevenue.toFixed(2)}%
                     </p>
                 ) : (
                     <p className="text-center text-red-600">
                         Total Revenue Decreased by&nbsp;
-                        {percentageChange?.changeTotalRevenue.toFixed(2)}%
+                        {changeTotalRevenue.toFixed(2)}%
                     </p>
                 )}
             </div>
@@ -47,26 +50,26 @@ export default function Revenue() {
             <div className="grid grid-cols-2 gap-2 w-full">
                 <div className="grid gap-2">
                     <div
-                        className={`rounded-xl bg-zinc-100 p-4 ${percentageChange?.changeProfit! > 0
+                        className={`rounded-xl bg-zinc-100 p-4 ${changeProfit > 0
                             ? "text-green-600"
                             : "text-red-600"
                             } w-full`}
                     >
                         <p className="text-center">Profits Change</p>
                         <p className="text-center">
-                            {percentageChange?.changeProfit! > 0 ? <span>&uarr;</span> : <span>&darr;</span>}
-                            {percentageChange?.changeProfit.toFixed(2)}
+                            {changeProfit > 0 ? <span>&uarr;</span> : <span>&darr;</span>}
+                            {changeProfit.toFixed(2)}
                         </p>
                     </div>
                     <div
-                        className={`rounded-xl bg-zinc-100 p-4 ${percentageChange?.changeLoss! < 0
+                        className={`rounded-xl bg-zinc-100 p-4 ${changeLoss < 0
                             ? "text-green-600"
                             : "text-red-600"
                             } w-full`}
                     >
                         <p className="text-center">Total Loss Change</p>
                         <p className="text-center">
-                            {percentageChange?.changeLoss.toFixed(2)}
+                            {changeLoss.toFixed(2)}
                         </p>
                     </div>
                 </div>
@@ -74,12 +77,12 @@ export default function Revenue() {
                     <div>
                         <p className="text-center">Total Revenue Change</p>
                         <p className="text-center">
-                            {percentageChange?.changeTotalRevenue! > 0 ? (
+                            {changeTotalRevenue > 0 ? (
                                 <span>&uarr;</span>
                             ) : (
                                 <span>&darr;</span>
                             )}
-                            {percentageChange?.changeTotalRevenue.toFixed(2)}
+                            {changeTotalRevenue.toFixed(2)}
                         </p>
                     </div>
                 </div>
